4ac1d8bc2413932c179f078faec03ff3e89e3ac9
cai.he <cai.he@wbkit.com>
2018-07-24 18:34:30 +0800

    scala map

--- src/main/scala/com/hochoy/scalatest/MapTest.scala
+++ src/main/scala/com/hochoy/scalatest/MapTest.scala
@@ -6,7 +6,24 @@ package com.hochoy.scalatest
   */
 object MapTest {
 
-    def main(args: Array[String]) {
+    def main(args: Array[String]): Unit = {
+        //testmap1()
+        testmap2()
+
+    }
+    def testmap2(): Unit ={
+        var a:Map[String,Int] = Map("k1"->1,"k2"->2)
+        a+=("k3"->3)
+        a.+=("k4"->4)
+        a.+=("k1"->100)
+        a.-=("k2")
+        List
+        println(a)
+        println(a.contains("k2"))
+        println(a.get("k2").getOrElse("default"))
+    }
+
+    def testmap1(){
         val scores = scala.collection.mutable.Map("Alice"->10,"Bob"->20)
         val bobS = scores("Bob")
         val bs = if (scores.contains("Bob")) scores("Bob") else 0

bebb3a202e75cc427a4747e204ee19a5588a8eae
cai.he <cai.he@wbkit.com>
2018-10-01 14:34:19 +0800

    rdd wc

--- src/main/scala/com/hochoy/spark/rdd/WordCount.scala
+++ src/main/scala/com/hochoy/spark/rdd/WordCount.scala
@@ -1,8 +1,13 @@
 package com.hochoy.spark.rdd
 
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
-  * Created by Cobub on 2018/9/15.
+  * Created by hochoy on 2018/9/15.
   */
 object WordCount {
 
+  val conf = new SparkConf().setMaster("local").setAppName("wc")
+  val sc = new SparkContext(conf)
+  val rdd = sc.parallelize(1 to 10)
 }

9d27ecdbf4007fe912db2d95540ea3a0ea8051d9
cai.he <cai.he@wbkit.com>
2018-10-02 09:39:30 +0800

    lazy

--- src/main/scala/com/hochoy/scalatest/other/LazyTest.scala
+++ src/main/scala/com/hochoy/scalatest/other/LazyTest.scala
@@ -6,10 +6,14 @@ package com.hochoy.scalatest.other
 class LazyTest {
 
 
-
 }
-object lazyOps{
-  def init():String = {
+
+/**
+  * Scala中使用关键字lazy来定义惰性变量，实现延迟加载(懒加载)。
+  * 惰性变量只能是不可变变量，并且只有在调用惰性变量时，才会去实例化这个变量
+  */
+object lazyOps {
+  def init(): String = {
     println("call init()...")
     return "ini...././././"
   }
@@ -18,5 +22,15 @@ object lazyOps{
      val prop = init()
     println("after init()...")
     println(prop)
+//    加了lazy关键字print的顺序：
+//    after init()...
+//    call init()...
+//    ini...././././
+//
+//    去掉lazy关键字print的顺序：
+//    call init()...
+//    after init()...
+//    ini...././././
+
   }
 }

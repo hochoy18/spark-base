bac767ab07ba0a3f65ee027cbf99cfce3555829b
cai.he <cai.he@wbkit.com>
2018-10-31 16:56:58 +0800

    JoinThread.

---
+++ src/main/java/com/hochoy/thread/JoinThread.java
@@ -0,0 +1,49 @@
+package com.hochoy.thread;
+
+/**
+ * join() 定义在Thread.java中。
+ * join() 的作用：让“主线程”等待“子线程”结束之后才能继续运行。
+ *
+ * 不使用join时 的输出结果：
+ * 先执行主线程再执行子线程
+ * *******************************************************************
+ *<<<<<<<<finish>>>>>>>>>    main
+ * start ...Join_T1
+ * finish....Join_T1
+ **********************************************************************
+ *
+ *
+ * 加入join之后输出结果
+ * **********************************************************************
+ * start ...Join_T1
+ * finish....Join_T1
+ * <<<<<<<<finish>>>>>>>>>    main
+ * *********************************************************************
+ */
+public class JoinThread {
+    public static void main(String[] args) throws Exception {
+        Thread joinA = new JoinThreadA("Join_T1 ");
+        joinA.start();
+
+        joinA.join();
+
+        System.out.println("<<<<<<<<finish>>>>>>>>>    "+ Thread.currentThread().getName());
+    }
+
+
+
+}
+
+class JoinThreadA extends Thread {
+    public JoinThreadA(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("start ..."+ this.getName());
+        for (int i=0;i<100000;i++)
+            ;
+        System.out.println("finish...."+this.getName());
+    }
+}

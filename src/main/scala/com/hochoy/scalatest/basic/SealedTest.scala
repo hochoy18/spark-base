adefe4a26fc608f5136715aa9a6dc12027392414
cai.he <cai.he@wbkit.com>
2018-11-13 10:58:31 +0800

    sealed

--- src/main/scala/com/hochoy/scalatest/basic/SealedTest.scala
+++ src/main/scala/com/hochoy/scalatest/basic/SealedTest.scala
@@ -3,8 +3,12 @@ package com.hochoy.scalatest.basic
 /**
   * Created by Cobub on 2018/10/9.
   * sealed 关键字有两个作用：
-  * 1.  其修饰的trait，class只能在当前文件里面被继承；
-  * 2.  在检查模式匹配的时候，用sealed修饰目的是让scala知道这些case的所有情况，
+  * 1.  防止继承滥用
+  *     其修饰的trait，class只能在当前文件里面被继承；
+  *     即：​sealed关键字可以修饰类和特质（特质）。密封类提供了一种约束：不能在类定义的文件之外定义任何新的子类
+  *
+  * 2.  模式匹配
+  *     在检查模式匹配的时候，用sealed修饰目的是让scala知道这些case的所有情况，
   * scala就能够在编译的时候进行检查，看你写的代码是否有没有漏掉什么没case到，减少编程的错误。
   */
 
@@ -13,6 +17,7 @@ sealed abstract class Person2
 case class Student2(name: String, sno: Int) extends Person2
 
 case class Teacher2(name: String, tno: Int) extends Person2
+
 
 object SealedTest extends App {
   val p: Person2 = Student2("yy", 21)

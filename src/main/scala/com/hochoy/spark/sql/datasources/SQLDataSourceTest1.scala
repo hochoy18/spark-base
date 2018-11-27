fcf474790e998afadd366e331fc2ad8043a4aa17
cai.he <cai.he@wbkit.com>
2018-11-27 15:21:24 +0800

    data source for spark sql

---
+++ src/main/scala/com/hochoy/spark/sql/datasources/SQLDataSourceTest1.scala
@@ -0,0 +1,151 @@
+package com.hochoy.spark.sql.datasources
+
+import com.hochoy.spark.utils.Constants._
+import com.hochoy.spark.utils.SparkUtils._
+import org.apache.spark.sql.SaveMode
+
+/**
+  *
+  *
+  * http://spark.apache.org/docs/latest/sql-data-sources-load-save-functions.html
+  *
+  * @note :  TODO
+  * @author :  hochoy
+  * @since :  Date : 2018年11月26日 19:34
+  * @version :  V1.0
+  */
+object SQLDataSourceTest1 {
+
+  val spark = createSparkSession("SQL Data Source ")
+
+  def main(args: Array[String]) {
+    import spark.implicits._
+
+    //  Generic Load/Save Functions
+    //    loadAndSave
+
+    //Run SQL on files directly
+    //    runSQLAndSave
+
+    //Loading Data Programmatically
+    //    runBasicParquetExample
+
+    //Schema Merging
+    runParquetSchemaMergingExample
+  }
+
+  def loadAndSave() = {
+    //  Generic Load/Save Functions
+    //  Manually Specifying Options
+    val userDF = spark.read.load(s"${USER_SPARK_PATH}${FILE_PATH}users.parquet")
+    val seleUserDF = userDF.select("name", "favorite_color")
+
+    val v = spark.read.parquet(s"${USER_SPARK_PATH}${FILE_PATH}users.parquet")
+    v.printSchema()
+    val vs = v.select("name", "favorite_numbers", "favorite_color")
+    vs.show()
+
+    seleUserDF.write.parquet(s"${USER_DIR}\\target\\nac.parquet")
+
+    val pDF = spark.read.format("json").load(s"${USER_SPARK_PATH}${FILE_PATH}sql_datasource.json")
+    val seleDF = pDF.select("name", "age")
+    seleDF.write.format("parquet").save(s"${USER_DIR}\\target\\r.parquet")
+
+    println("===================================load data from manually-save file and vie regex-path =====================================")
+    val getDF = spark.read.format("parquet").parquet(s"${USER_DIR}\\target\\r.parquet\\part-*.parquet")
+    getDF.printSchema()
+    getDF.select("name", "age").show()
+
+
+    println("===================================Manually Specifying Options   -- csv =====================================")
+    //  Manually Specifying Options
+    //http://spark.apache.org/docs/latest/sql-data-sources-load-save-functions.html#manually-specifying-options
+    val peopleDF = spark.read.format(FORMAT_CSV)
+      .option("sep", ";")
+      .option("inferSchema", "true")
+      .option("header", "true").load(s"${USER_SPARK_PATH}${FILE_PATH}people.csv")
+
+    peopleDF.printSchema()
+    val p = peopleDF.select("name", "age", "job")
+    p.show()
+
+    //    println("===================================Manually Specifying Options   -- orc  =====================================")
+    //
+    //    vs.show()
+    //    vs.write.format(FORMAT_ORC)
+    //      .option("orc.bloom.filter.columns", "favorite_color")
+    //      .option("orc.dictionary.key.threshold", "1.0")
+    //      .save(s"${USER_DIR}\\target\\vs.orc")
+
+  }
+
+  def runSQLAndSave() = {
+    //Run SQL on files directly
+    val sqlDF = spark.sql(s"select * from parquet.`${USER_SPARK_PATH}${FILE_PATH}users.parquet`")
+    sqlDF.printSchema()
+    sqlDF.show()
+
+    //Save Modes
+    //http://spark.apache.org/docs/latest/sql-data-sources-load-save-functions.html#save-modes
+    /**
+      * Optional save mode
+      *
+      * SaveMode.ErrorIfExists
+      * SaveMode.Append
+      * SaveMode.Overwrite
+      * SaveMode.Ignore
+      */
+    sqlDF.write.mode(SaveMode.Append).format(FORMAT_JSON).save(s"${USER_DIR}\\target\\saveMode.json")
+
+    val resDF = spark.read.json(s"${USER_DIR}\\target\\saveMode.json\\part-*.json")
+    resDF.printSchema()
+    resDF.select("favorite_color", "favorite_numbers", "name").show()
+
+  }
+
+  def runBasicParquetExample = {
+
+    //Loading Data Programmatically
+    /**
+      * http://spark.apache.org/docs/latest/sql-data-sources-parquet.html#loading-data-programmatically
+      *
+      */
+    val peopleDF = spark.read.json(s"${USER_SPARK_PATH}${FILE_PATH}people.json")
+    peopleDF.write.mode(SaveMode.Append).parquet(s"${TARGET_DIR}\\peo.parquet")
+    val resDF = spark.read.parquet(s"${TARGET_DIR}\\peo.parquet")
+    resDF.createOrReplaceTempView("peo_p")
+    val nameDF = spark.sql("select name from peo_p ")
+    nameDF.printSchema()
+    nameDF.show()
+  }
+
+  def runParquetSchemaMergingExample = {
+
+    //Schema Merging
+    /**
+      * http://spark.apache.org/docs/latest/sql-data-sources-parquet.html#schema-merging
+      */
+    import spark.implicits._
+    val path = {
+      s"${TARGET_DIR}\\schema_merging.parquet"
+    }
+    val squaresDF = spark.sparkContext.makeRDD(1 to 10).map(i => (i, Math.pow(i, 2))).toDF("value", "square")
+    squaresDF.write.mode(SaveMode.Overwrite)parquet(s"${path}\\key=1")
+
+    val cobesDF = spark.sparkContext.makeRDD(1 to 10).map(i=>(i, Math.pow(i, 3))).toDF("value", "cube")
+    cobesDF.write.mode(SaveMode.Overwrite).parquet(s"${path}\\key=2")
+
+    //before merge
+    println("===================================before merge =====================================")
+    val v = spark.read.parquet(path)
+    v.printSchema()
+    v.select("value","key","square").show()
+
+    println("===================================after merge =====================================")
+    val mergedDF = spark.read.option("mergeSchema", "true").parquet(path)
+    mergedDF.select("value","key","square","cube").show()
+    mergedDF.printSchema()
+
+
+  }
+}
--- src/main/scala/com/hochoy/spark/utils/Constants.scala
+++ src/main/scala/com/hochoy/spark/utils/Constants.scala
@@ -8,9 +8,28 @@ import com.hochoy.spark.utils.SparkUtils._
   */
 object Constants {
 
-  val USER_SPARK_PATH ={
+  val FORMAT_CSV: String = "csv"
+  val FORMAT_PARQUET: String = "parquet"
+  val FORMAT_JSON: String = "json"
+  val FORMAT_JDBC = "jdbc"
+  val FORMAT_ORC: String = "orc"
+  val FORMAT_TEXT: String = "text"
+  val FORMAT_LIBSVM: String = "libsvm"
+
+
+  val USER_DIR = {
     hadoopHomeSet
-    System.getProperty("user.dir")+"\\src\\main\\scala\\com\\hochoy\\spark\\"
+    System.getProperty("user.dir")
   }
-  val FILE_PATH={"sql\\data\\"}
+
+  val TARGET_DIR = {
+    USER_DIR + "\\target"
+  }
+
+  val USER_SPARK_PATH = {
+    USER_DIR + "\\src\\main\\scala\\com\\hochoy\\spark\\"
+  }
+  val FILE_PATH = {
+    "sql\\data\\"
+  }
 }

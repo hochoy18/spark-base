d32cf23c3d25e1a94851cdad13da48f1b2aee27b
cai.he <cai.he@wbkit.com>
2019-02-28 14:11:52 +0800

    clickhouse performance test

--- src/main/java/com/hochoy/clickhouse/ClickHouseJDBC.java
+++ src/main/java/com/hochoy/clickhouse/ClickHouseJDBC.java
@@ -14,60 +14,130 @@ import java.util.Map;
  * @date 2019/2/26
  */
 public class ClickHouseJDBC {
-    static String address = "jdbc:clickhouse://192.168.1.240:8123/default";
-    static String driver = "ru.yandex.clickhouse.ClickHouseDriver";
+    private static String address = "jdbc:clickhouse://192.168.1.240:8123/default";
+    private static String driver = "ru.yandex.clickhouse.ClickHouseDriver";
+
+    private static String allUser = "select count(1) as ALL_USER_COUNT from (" +
+            "select distinct deviceid from action_data_c4j_all  " +
+            "where appkey = '33645ea6eda746b3be4cfc5fa23ec6a6'" +
+            ")";// 特定应用下所有 用户
+    private static String activeUser = "select count(*) as ACTIVE_USER_OF_DAY from (" +
+            "select distinct deviceid  from action_data_c4j_all  " +
+            "where appkey = '33645ea6eda746b3be4cfc5fa23ec6a6' " +
+            "and time >= '2018-09-03 00:00:00' and time <'2018-09-04 00:00:00'" +
+            ");"; // 特定应用下 2018-09-03 的活跃用户
+
+    private static String sql_nanjing = "select count(1) as ALL_USER_COUNT_Nanjing from (" +
+            "select distinct deviceid from action_data_c4j_all  " +
+            "where appkey = '33645ea6eda746b3be4cfc5fa23ec6a6' " +
+            "and  country = '中国' and region = '江苏' and city = '南京'" +
+            ")"; //应用下 南京 所有 用户
+
+    private static String launch_count = "select count(*) as LAUNCH_COUNT from action_data_c4j_all  " +
+            "where appkey = '33645ea6eda746b3be4cfc5fa23ec6a6' " +
+            "and  action = '$launch' " +
+            "and time >= '2018-09-03 00:00:00' and time <'2018-09-04 00:00:00';"; // 特定应用下 2018-09-03 的启动次数
+
+    private static String login_count = "select count(*) as LOGIN_COUNT from action_data_c4j_all  " +
+            "where appkey = '33645ea6eda746b3be4cfc5fa23ec6a6' " +
+            "and  action like  'login%' " +
+            "and time >= '2018-09-03 00:00:00' and time <'2018-09-04 00:00:00'";
+    private static String group_by_version_channelid = "select count(*) as GROUP_BY_VERSION_CHANNELID , version ,channelid   from action_data_c4j_all  " +
+            "where appkey = '33645ea6eda746b3be4cfc5fa23ec6a6' " +
+            "and  action = '$launch'  and platform = 'android' " +
+            "and time >= '2018-09-03 00:00:00' and time <'2018-09-04 00:00:00' " +
+            "group by  version ,channelid";
+
+
+    private static String group_by_module = "select count(*) as GROUP_BY_MODULE, module  from action_data_c4j  " +
+            "where appkey = '33645ea6eda746b3be4cfc5fa23ec6a6'  " +
+            "and time >= '2018-09-03 00:00:00' and time <'2018-10-04 00:00:00'  " +
+            "group by module";
+
+    private static String avg_duration = " select round(avg(duration)) as ROUND_AVG  from action_data_c4j  " +
+            "where appkey = '33645ea6eda746b3be4cfc5fa23ec6a6'  " +
+            "and time >= '2018-09-03 00:00:00' and time <'2018-10-04 00:00:00'";
+
+    private static String group_by_version_channelid_having = "select count(*) as GROUP_BY_VERSION_CHANNELID_HAVING, version ,channelid   from action_data_c4j_all  " +
+            "where appkey = '33645ea6eda746b3be4cfc5fa23ec6a6' and  action = '$launch'  and platform = 'android' " +
+            "and time >= '2018-09-03 00:00:00' and time <'2018-09-04 00:00:00' " +
+            "group by  version ,channelid having GROUP_BY_VERSION_CHANNELID_HAVING > 50 and channelid like '1000%'";
     public static void main(String[] args) {
-//        ru.yandex.clickhouse.ClickHouseDriver
-        String sqlDB = "show databases";//查询数据库
-        String sqlTab = "show tables";//查看表
-        String sqlCount = "select count(*) count from action_data_all";//查询ontime数据量
-        exeSql(sqlDB);
-        exeSql(sqlTab);
-        exeSql(sqlCount);
+        connection = init();
+        exeSql(allUser);
+        exeSql(activeUser);
+        exeSql(sql_nanjing);
+        exeSql(launch_count);
+        exeSql(login_count);
+        exeSql(avg_duration);
+        exeSql(group_by_version_channelid);
+        exeSql(group_by_version_channelid_having);
+        exeSql(group_by_module);
+        close();
     }
 
+    private static Connection connection = null;
 
-    public static void exeSql(String sql){
-
-        Connection connection = null;
+    private  static void exeSql(String sql) {
         Statement statement = null;
         ResultSet results = null;
         try {
-            Class.forName(driver);
-            connection = DriverManager.getConnection(address);
             statement = connection.createStatement();
             long begin = System.currentTimeMillis();
             results = statement.executeQuery(sql);
             long end = System.currentTimeMillis();
-            System.out.println("执行（"+sql+"）耗时："+(end-begin)+"ms");
-            ResultSetMetaData rsmd = results.getMetaData();
-            List<Map> list = new ArrayList();
-            while(results.next()){
-                Map map = new HashMap();
-                for(int i = 1;i<=rsmd.getColumnCount();i++){
-                    map.put(rsmd.getColumnName(i),results.getString(rsmd.getColumnName(i)));
-                }
-                list.add(map);
-            }
-            for(Map map : list){
-                System.err.println(map);
-            }
+            System.out.println("耗时："+(end - begin) + "ms" + "  执行（" + sql + "）");
+//            ResultSetMetaData rsmd = results.getMetaData();
+//            List<Map> list = new ArrayList();
+//            while (results.next()) {
+//                Map map = new HashMap();
+//                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+//                    map.put(rsmd.getColumnName(i), results.getString(rsmd.getColumnName(i)));
+//                }
+//                list.add(map);
+//            }
+//            for (Map map : list) {
+//                System.err.println(map);
+//            }
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {//关闭连接
+        } finally {//关闭连接
             try {
-                if(results!=null){
+                if (results != null) {
                     results.close();
                 }
-                if(statement!=null){
+                if (statement != null) {
                     statement.close();
                 }
-                if(connection!=null){
-                    connection.close();
-                }
+
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+        }
+//        try {
+//            Thread.sleep(1000);
+//        }catch (Exception e){
+//
+//        }
+    }
+
+    private static Connection init() {
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(address);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private static void close() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }

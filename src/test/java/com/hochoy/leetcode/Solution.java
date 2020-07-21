54ba88879876385bb9d8fade111a4a944bcb066d
cai.he <cai.he@wbkit.com>
2020-07-21 09:50:52 +0800

    leetcode

--- src/test/java/com/hochoy/leetcode/Solution.java
+++ src/test/java/com/hochoy/leetcode/Solution.java
@@ -1858,8 +1858,68 @@ public class Solution {
 
         return 0;
     }
+    @Test
+    public void search() {
+        int[] nums ;
+        int target;
+        nums = new int[]{5,7,7,8,8,10};
+        target = 8;
+        int search;
+        search = search(nums, target);
+        System.out.println(search);
+        assertEquals(2,search);
 
+        nums = new int[]{5,7,7,8,8,10};
+        target = 6;
+        search = search(nums, target);
+        System.out.println(search);
+        assertEquals(0,search);
 
+        nums = new int[]{6,6,6,6,6,6};
+        target = 6;
+        search = search(nums, target);
+        System.out.println(search);
+        assertEquals(6,search);
+    }
+
+    public int search(int[] nums, int target) {
+        int idx = firstIndex(nums,target);
+
+        if (idx == -1)
+            return 0;
+
+        int res = 1;
+        for (int i = idx - 1; i >= 0; i--) {
+            if (nums[i] == target)
+                res ++;
+            else break;
+        }
+        for (int i = idx +1; i < nums.length; i++) {
+            if (nums[i] == target)
+                res ++;
+            else break;
+
+        }
+
+        return res;
+    }
+    int firstIndex(int[] nums ,int target){
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end ){
+            int mid = (start + end ) >> 1;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target) {
+                end = mid - 1;
+
+            }
+            else if (nums[mid] < target) {
+                start = mid + 1;
+            }
+        }
+        return  -1;
+    }
 
 }
 

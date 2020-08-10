e800ab9af8b4ee0ef5dfa2117615393001dc8f32
cai.he <cai.he@wbkit.com>
2020-08-10 15:18:56 +0800

    'bst'

--- src/test/java/com/hochoy/leetcode/Solution.java
+++ src/test/java/com/hochoy/leetcode/Solution.java
@@ -2861,6 +2861,36 @@ public class Solution {
     }
 
     @Test
+    public void searchBST() {
+
+    }
+
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null){
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (! queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if (node.val == val){
+                return node;
+            }
+            if (node.left != null){
+                queue.offer(node.left);
+            }
+            if(node.right != null){
+                queue.offer(node.right);
+            }
+
+        }
+        return null;
+
+    }
+
+    @Test
     public void getKth() {
 
     }

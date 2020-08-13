b10334c6204e881598b3bbdcf2488933800f5ed3
cai.he <cai.he@wbkit.com>
2020-08-13 09:45:57 +0800

    kafka

--- src/main/java/com/hochoy/kafka/KafkaProducerAnalysis.java
+++ src/main/java/com/hochoy/kafka/KafkaProducerAnalysis.java
@@ -1,136 +1,67 @@
 package com.hochoy.kafka;
 
-import com.hochoy.utils.HochoyConstants;
-import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.hochoy.utils.HochoyUtils;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
-import java.util.Random;
-import java.util.concurrent.*;
-
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class KafkaProducerAnalysis {
 
-    private static final Logger logger = LoggerFactory.getLogger(KafkaProducerAnalysis.class);
 
-    static int MSG_SIZE = (int)1e2;
-    static ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    static CountDownLatch countDownLatch = new CountDownLatch(MSG_SIZE);
-
-
-
-    public static Properties initConfig(){
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, HochoyConstants.KafkaConstants.BROKER_LIST);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.CLIENT_ID_CONFIG,"producer.client.id.demo");
-        return props;
-
-    }
-
-
-
-
-
+    static int MSG_SIZE = (int)1e4;
+    static String TOPIC = "test_topic";
 
     public static void main(String[] args) {
 
-        Properties props = initConfig();
-        Random random = new Random();
+        Properties props = HochoyUtils.getProperties("producer.properties");
+
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
+        String value = "{\"data\":[{\"country\":\"局域网\",\"request\":\"POST /action HTTP/1.1\",\"productid\":\"1\",\"city\":\"\",\"latitude\":\"31.1716369000\",\"ip\":\"192.168.1.243\",\"useragent\":\"Python-urllib/2.7\",\"servertime\":\"2020-03-04 18:30:03.000\",\"sessionid\":\"57c3b7ac4e5f4b22b710b3d06ee9faff\",\"lib_version\":\"1.0.0\",\"userid\":\"20000178769\",\"deviceid\":\"iOS_DEVICEID_4362806746292507\",\"uuid\":\"e6bc377f84c450767e2730e0a70653f1\",\"actionattach\":{},\"action\":\"$exitPage\",\"appkey\":\"23924869c35a4108b2539412fdf9c6f8\",\"clienttime\":\"2020-03-03 18:30:03.971\",\"region\":\"局域网\",\"properties\":{\"screen_width\":136,\"utm_campaign\":\"国庆节大促\",\"utm_medium\":\"TopBanner\",\"language\":\"lt_LT\",\"version\":\"2.1\",\"platform\":\"ios\",\"network\":\"WIFI\",\"manufacturer\":\"apple\",\"duration\":80087,\"screen_height\":1024,\"mccmnc\":\"46003\",\"is_new_device\":\"false\",\"refer\":\"\",\"is_update\":\"false\",\"model\":\"iPad4,1\",\"pagetitle\":\"PageTitle_897156978712\",\"page\":\"RegistviewController\",\"osversion\":\"3.1.2\",\"channelid\":\"baidu\",\"utm_content\":\"0815-method\",\"utm_source\":\"百度\"},\"longitude\":\"121.3368797000\"}]}";
 
         for (int i = 0 ; i < MSG_SIZE; i ++ ){
 
-            ProducerRecord<String, String> record = new ProducerRecord<>(HochoyConstants.KafkaConstants.TOPIC, random.nextInt(3), i+"", "{\"data\":[{\"country\":\"局域网\",\"request\":\"POST /action HTTP/1.1\",\"productid\":\"1\",\"city\":\"\",\"latitude\":\"31.1716369000\",\"ip\":\"192.168.1.243\",\"useragent\":\"Python-urllib/2.7\",\"servertime\":\"2020-03-04 18:30:03.000\",\"sessionid\":\"57c3b7ac4e5f4b22b710b3d06ee9faff\",\"lib_version\":\"1.0.0\",\"userid\":\"20000178769\",\"deviceid\":\"iOS_DEVICEID_4362806746292507\",\"uuid\":\"e6bc377f84c450767e2730e0a70653f1\",\"actionattach\":{},\"action\":\"$exitPage\",\"appkey\":\"23924869c35a4108b2539412fdf9c6f8\",\"clienttime\":\"2020-03-03 18:30:03.971\",\"region\":\"局域网\",\"properties\":{\"screen_width\":136,\"utm_campaign\":\"国庆节大促\",\"utm_medium\":\"TopBanner\",\"language\":\"lt_LT\",\"version\":\"2.1\",\"platform\":\"ios\",\"network\":\"WIFI\",\"manufacturer\":\"apple\",\"duration\":80087,\"screen_height\":1024,\"mccmnc\":\"46003\",\"is_new_device\":\"false\",\"refer\":\"\",\"is_update\":\"false\",\"model\":\"iPad4,1\",\"pagetitle\":\"PageTitle_897156978712\",\"page\":\"RegistviewController\",\"osversion\":\"3.1.2\",\"channelid\":\"baidu\",\"utm_content\":\"0815-method\",\"utm_source\":\"百度\"},\"longitude\":\"121.3368797000\"}]}");
+//            ProducerRecord<String, String> record = new ProducerRecord<>( TOPIC, random.nextInt(3), i+"", "{\"data\":[{\"country\":\"局域网\",\"request\":\"POST /action HTTP/1.1\",\"productid\":\"1\",\"city\":\"\",\"latitude\":\"31.1716369000\",\"ip\":\"192.168.1.243\",\"useragent\":\"Python-urllib/2.7\",\"servertime\":\"2020-03-04 18:30:03.000\",\"sessionid\":\"57c3b7ac4e5f4b22b710b3d06ee9faff\",\"lib_version\":\"1.0.0\",\"userid\":\"20000178769\",\"deviceid\":\"iOS_DEVICEID_4362806746292507\",\"uuid\":\"e6bc377f84c450767e2730e0a70653f1\",\"actionattach\":{},\"action\":\"$exitPage\",\"appkey\":\"23924869c35a4108b2539412fdf9c6f8\",\"clienttime\":\"2020-03-03 18:30:03.971\",\"region\":\"局域网\",\"properties\":{\"screen_width\":136,\"utm_campaign\":\"国庆节大促\",\"utm_medium\":\"TopBanner\",\"language\":\"lt_LT\",\"version\":\"2.1\",\"platform\":\"ios\",\"network\":\"WIFI\",\"manufacturer\":\"apple\",\"duration\":80087,\"screen_height\":1024,\"mccmnc\":\"46003\",\"is_new_device\":\"false\",\"refer\":\"\",\"is_update\":\"false\",\"model\":\"iPad4,1\",\"pagetitle\":\"PageTitle_897156978712\",\"page\":\"RegistviewController\",\"osversion\":\"3.1.2\",\"channelid\":\"baidu\",\"utm_content\":\"0815-method\",\"utm_source\":\"百度\"},\"longitude\":\"121.3368797000\"}]}");
 
-            executorService.submit(new ProducerWorker(record,producer));
-        }
-//        try {
-//            countDownLatch.await();
-//
-//        }catch (InterruptedException e ){
-//            e.printStackTrace();
-//        }
-
-
-//        int j = 0;
-//        while (j < 1e6){
-//            j++;
-//            int i = random.nextInt();
-//            ProducerRecord<String, String> record = new ProducerRecord<>(HochoyConstants.KafkaConstants.TOPIC, random.nextInt(3), i+"", "{\"data\":[{\"country\":\"局域网\",\"request\":\"POST /action HTTP/1.1\",\"productid\":\"1\",\"city\":\"\",\"latitude\":\"31.1716369000\",\"ip\":\"192.168.1.243\",\"useragent\":\"Python-urllib/2.7\",\"servertime\":\"2020-03-04 18:30:03.000\",\"sessionid\":\"57c3b7ac4e5f4b22b710b3d06ee9faff\",\"lib_version\":\"1.0.0\",\"userid\":\"20000178769\",\"deviceid\":\"iOS_DEVICEID_4362806746292507\",\"uuid\":\"e6bc377f84c450767e2730e0a70653f1\",\"actionattach\":{},\"action\":\"$exitPage\",\"appkey\":\"23924869c35a4108b2539412fdf9c6f8\",\"clienttime\":\"2020-03-03 18:30:03.971\",\"region\":\"局域网\",\"properties\":{\"screen_width\":136,\"utm_campaign\":\"国庆节大促\",\"utm_medium\":\"TopBanner\",\"language\":\"lt_LT\",\"version\":\"2.1\",\"platform\":\"ios\",\"network\":\"WIFI\",\"manufacturer\":\"apple\",\"duration\":80087,\"screen_height\":1024,\"mccmnc\":\"46003\",\"is_new_device\":\"false\",\"refer\":\"\",\"is_update\":\"false\",\"model\":\"iPad4,1\",\"pagetitle\":\"PageTitle_897156978712\",\"page\":\"RegistviewController\",\"osversion\":\"3.1.2\",\"channelid\":\"baidu\",\"utm_content\":\"0815-method\",\"utm_source\":\"百度\"},\"longitude\":\"121.3368797000\"}]}");
-//            Future<RecordMetadata> future = producer.send(record, new Callback() {
-//                @Override
-//                public void onCompletion(RecordMetadata metadata, Exception exception) {
-//                    if (exception != null){
-//                        exception.printStackTrace();
-//                    }else {
-//                        System.out.printf("topic : %s%n partition : %s%n offset : %s%n key : %s%n value : %s%n------------------------------------------------",
-//                                metadata.topic() ,
-//                                metadata.partition() ,
-//                                metadata.offset(),
-//                                record.key(),
-//                                record.value());
-//                    }
-//                }
-//            });
-//            try {
-//                RecordMetadata metadata = future.get();
-//                String topic = metadata.topic();
-//                int partition = metadata.partition();
-//                long offset = metadata.offset();
-//                System.out.println(topic);
-//                System.out.println(partition);
-//                System.out.println(offset);
-//                System.out.println("--------------------");
-////                Thread.sleep(1000 / 100);
-//            }catch (InterruptedException| ExecutionException e ){
-//                logger.error("e:{}",e);
-//            }finally {
-//                producer.flush();
-//            }
-//
-//
-//        }
-
-    }
-
-
-   static class ProducerWorker implements Runnable {
-
-        ProducerRecord<String,String> record;
-        KafkaProducer<String,String> producer;
-
-        public ProducerWorker(ProducerRecord<String, String> record, KafkaProducer<String, String> producer) {
-            this.record = record;
-            this.producer = producer;
-        }
-
-        @Override
-        public void run() {
-            final String id = Thread.currentThread().getId() + "-" + System.identityHashCode(producer);
-            producer.send(record, new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata metadata, Exception exception) {
-                    if (null != exception)
-                        exception.printStackTrace();
-                    if (null != metadata)
-                        System.out.printf("id : %s%n offset : %s%n partition : %s%n ",
-                                id,metadata.offset(),metadata.partition()
-                        );
+            value = "kkkkkkkkkkkk" + i;
+            System.out.println(value);
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC,value);
+            Future<RecordMetadata> future = producer.send(record, (metadata, exception) -> {
+                if (exception == null) {
+                    System.out.println("Msg send success !!!! ");
+                    long offset = metadata.offset();
+                    int partition = metadata.partition();
+                    System.out.printf("partition : %d , offset : %d  %n", partition, offset);
+                } else {
+                    exception.printStackTrace();
                 }
-            });
-            System.out.printf("%s : 数据[%s-%s]已发送",id,record.key(),record.value());
 
-            countDownLatch.countDown();
+            });
+            RecordMetadata recordMetadata = null;
+            try {
+                recordMetadata = future.get();
+                long offset = recordMetadata.offset();
+                System.out.println("=========================="+ offset);
+//                if (i % 1000 == 0){
+                    HochoyUtils.sleep(500);
+//                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
 
 
 
         }
+
     }
+
+
 }
 
 
--- src/main/java/com/hochoy/utils/HochoyUtils.java
+++ src/main/java/com/hochoy/utils/HochoyUtils.java
@@ -1,5 +1,10 @@
 package com.hochoy.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.*;
+
 public class HochoyUtils {
     private HochoyUtils() {
     }
@@ -9,6 +14,170 @@ public class HochoyUtils {
             Thread.sleep(ms);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static Properties getProperties(String fileName) {
+        Properties prop = new Properties();
+        InputStream in;
+        try {
+            in = HochoyUtils.class.getClassLoader().getResourceAsStream(fileName);
+
+            prop.load(in);
+            for (Map.Entry<Object, Object> entry : prop.entrySet()) {
+                System.out.println(entry.getKey() + "=" + entry.getValue());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+
+    public static Connection getMySQLConn(String fileName) {
+        Properties properties = getProperties(fileName);
+        Connection conn = null;
+        try {
+            Class.forName(properties.getProperty("driver.class")).newInstance();
+            conn = DriverManager.getConnection(properties.getProperty("jdbc.url"), properties.getProperty("jdbc.username"), properties.getProperty("jdbc.password"));
+//
+//            PreparedStatement preparedStatement = conn.prepareStatement(sql,args);
+//            for (int i = 1; i <= args.length; i++) {
+//                preparedStatement.setString(i,args[i-1]);
+//            }
+//
+//            preparedStatement.executeUpdate();
+//
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            resultSet.get
+//            while (resultSet.next()){
+//
+//            }
+
+        } catch (InstantiationException | SQLException | IllegalAccessException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static int update(Connection conn, String sql, Object... args) {
+
+        PreparedStatement statement = null;
+        int execute = 0;
+        try {
+            statement = conn.prepareStatement(sql);
+            for (int i = 1; i <= args.length; i++) {
+                statement.setObject(i, args[i - 1]);
+            }
+            execute = statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return execute;
+    }
+
+    public static long getNums(Connection conn, String sql, Object... args) {
+
+        List<Map<String, Object>> query = query(conn, sql, args);
+        if (query == null || query.isEmpty())
+            return 0;
+
+        Object[] values = query.get(0).values().toArray();
+        return Long.parseLong(values[0].toString());
+    }
+
+    public static Map<String, Object> findOne(Connection conn, String sql, Object... args) {
+
+        List<Map<String, Object>> query = query(conn, sql, args);
+        if (query == null || query.isEmpty())
+            return new HashMap<>();
+        return query.get(0);
+    }
+
+    public static List<Map<String, Object>> query(Connection conn, String sql, Object... args) {
+        List<Map<String, Object>> res = new ArrayList<>();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = conn.prepareStatement(sql);
+            for (int i = 1; i <= args.length; i++) {
+                preparedStatement.setObject(i, args[i - 1]);
+            }
+
+            resultSet = preparedStatement.executeQuery();
+
+
+            while (resultSet.next()) {
+                Map<String, Object> map = new HashMap<>();
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int columnCount = metaData.getColumnCount();
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    String columnClassName = metaData.getColumnClassName(i);
+                    System.out.println(columnClassName);
+                    Object object = resultSet.getObject(i);
+                    map.put(columnName, object);
+                }
+                res.add(map);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        return res;
+
+    }
+
+    public static void close(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Connection mySQLConn = getMySQLConn("mysql.properties");
+
+        List<Map<String, Object>> query = query(mySQLConn, "select * from mysql_offset");
+        for (Map<String, Object> map : query) {
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                System.out.printf(" %s : %s", entry.getKey(), entry.getValue());
+            }
+            System.out.println();
+        }
+
+        int update = update(mySQLConn, "insert into mysql_offset values ('111','11',1,1,1)");
+        System.out.println(update);
+
+        query = query(mySQLConn, "select * from mysql_offset");
+        for (Map<String, Object> map : query) {
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                System.out.printf(" %s : %s", entry.getKey(), entry.getValue());
+            }
+            System.out.println();
         }
     }
 }
